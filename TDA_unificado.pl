366c2e90d984c0990224b7e6157fcc2c5cac7730
Andres_Zelaya <andres.zelaya.d@hotmail.com>
2022-10-11 16:15:19 -0300

    Implementacion de verificadores

--- TDA_unificado.pl
+++ TDA_unificado.pl
@@ -3,7 +3,7 @@
 
 
 
-%---------------------------------------------------------------
+%-----------------------------------------------------------------
 % Pixbit
 % [PosX, PosY, Bit, Depth]
 pixbitd(PosX, PosY, Bit, Depth,[PosX, PosY, Bit, Depth]):-
@@ -12,7 +12,7 @@ pixbitd(PosX, PosY, Bit, Depth,[PosX, PosY, Bit, Depth]):-
     integer(Bit),
     Bit == 0 ; Bit == 1,
     integer(Depth).
-% --------------------------------------------------------------
+% ----------------------------------------------------------------
 % Pixrgb
 % [PosX, PosY, R, G, B, Depth]
 pixrgb(PosX, PosY, R, G, B, Depth,[PosX, PosY, R, G, B, Depth]):-
@@ -25,7 +25,7 @@ pixrgb(PosX, PosY, R, G, B, Depth,[PosX, PosY, R, G, B, Depth]):-
     integer(B),
     B >= 0 ; B =< 255,
     integer(Depth).
-% --------------------------------------------------------------
+% ----------------------------------------------------------------
 % Pixhex
 % [PosX, PosY, Hex, Depth]
 pixhex(PosX, PosY, Hex, Depth,[PosX, PosY, Hex, Depth]):-
@@ -33,4 +33,33 @@ pixhex(PosX, PosY, Hex, Depth,[PosX, PosY, Hex, Depth]):-
     integer(PosY),
     string(Hex),
     integer(Depth).
-% --------------------------------------------------------------
+% ----------------------------------------------------------------
+
+% Constructor de imagen
+image(Largo, Ancho, Pixeles, [Largo, Ancho, Pixeles]).
+
+% Verificador Bitmap
+imageIsBitmap([]).
+imageIsBitmap([Pixbitd | Cdr]) :-
+    pixbitd(_, _, ContBit, _, Pixbitd),
+    ContBit = 0 ; ContBit = 1,
+    imageIsBitmap(Cdr).
+
+% Verificador Pixmap
+imageIsPixmap([]).
+imageIsPixmap([Pixrgbd | Cdr]) :-
+    pixrgbd(_, _, R, G, B, _, Pixrgbd),
+    integer(R),
+    R >= 0 ; R =< 255,
+    integer(G),
+    G >= 0 ; G =< 255,
+    integer(B),
+    B >= 0 ; B =< 255,
+    imageIsPixmap(Cdr).
+
+% Verificador Hexmap
+imageIsHexmap([]).
+imageIsHexmap([Pixhexd | Cdr]) :-
+    pixbitd(_, _, ContHex, _, Pixhexd),
+    string(ContHex),
+    imageIsHexmap(Cdr).
